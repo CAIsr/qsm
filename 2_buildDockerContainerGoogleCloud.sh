b4b04b79bbeb9ed01103330a45d9148f378c13df
Steffen Bollmann <steffen.bollmann@cai.uq.edu.au>
2017-11-09 22:10:54 +1000

    doesnt work yet

--- 2_buildDockerContainerGoogleCloud.sh
+++ 2_buildDockerContainerGoogleCloud.sh
@@ -1,5 +1,7 @@
-export PROJECT_ID="$(gcloud config get-value project -q)"
+gcloud container builds submit --tag gcr.io/dicom2cloud/tgv-qsm:v20171109 . --timeout=2h15m5s
 
-docker build -t gcr.io/${PROJECT_ID}/singularity-tgv-qsm:v1 .
-
-docker images
+#Build in shell:
+#export PROJECT_ID="$(gcloud config get-value project -q)"
+#docker build -t gcr.io/${PROJECT_ID}/singularity-tgv-qsm:v1 .
+#
+#docker images
--- 3_buildSingularityContainer.sh
+++
@@ -1,3 +0,0 @@
-singularity create --size 12000 tgv.img
-sudo singularity bootstrap tgv.img Singularity
-

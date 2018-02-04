17c0e31452980f12c5e7e47fa92d63a99446e2b2
Steffen <stebo85@web.de>
2018-02-04 19:35:24 +1000

    on the way to working neurodocker

--- 1_createDockerFile.sh
+++
@@ -1,4 +0,0 @@
-
-#pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --user
-
-neurodocker generate --base debian:stretch --pkg-manager apt --fsl version=5.0.10 > Dockerfile_neurodocker

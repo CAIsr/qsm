87cf08b05d56629c9bec3791fdd22b06b8a73b0a
Steffen <stebo85@web.de>
2018-02-04 21:46:39 +1000

    added tgv qsm to neurodocker command

--- 1_runNeurodocker.sh
+++ 1_runNeurodocker.sh
@@ -2,4 +2,15 @@
 #install neurodocker
 #pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --user
 
-neurodocker generate --base debian:stretch --pkg-manager apt --fsl version=5.0.10 --dcm2niix version='latest' > Dockerfile_neurodocker
+neurodocker generate \
+	--base debian:stretch \
+	--pkg-manager apt \
+	--fsl version=5.0.10 \
+	--dcm2niix version='latest' \
+	--install wget unzip python3 python-numpy python-nibabel python-setuptools cython \
+	--workdir /\
+	--run="wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip" \
+	--run="unzip /TGVQSM-plus.zip" \
+	--workdir /TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3 \
+	--run="python setup.py install" \
+	> Dockerfile_neurodocker
--- 2_buildDockerContainerGoogleCloud.sh
+++
@@ -1,7 +0,0 @@
-gcloud container builds submit --tag gcr.io/dicom2cloud/tgv-qsm:v20171109 . --timeout=2h15m5s
-
-#Build in shell:
-#export PROJECT_ID="$(gcloud config get-value project -q)"
-#docker build -t gcr.io/${PROJECT_ID}/singularity-tgv-qsm:v1 .
-#
-#docker images

78ab0c3cdd5086b957bdc7f08f3656b9c64a0993
Steffen <stebo85@web.de>
2018-02-05 20:58:03 +1000

    switched ubuntu latest to 17.04 to avoid problem

--- 1_neurodocker_gen_fsl.sh
+++ 1_neurodocker_gen_fsl.sh
@@ -3,7 +3,7 @@
 #pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --user
 
 neurodocker generate \
-	--base ubuntu:latest \
+	--base ubuntu:17.04 \
 	--pkg-manager apt \
 	--fsl version=5.0.10 \
 	--dcm2niix version='latest' \

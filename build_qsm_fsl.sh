1a1eb6c101a55fe324e564a76fec94b205a9a8f6
Steffen <stebo85@web.de>
2018-07-16 15:13:30 +1000

    qsm and fsl work

--- build_qsm_fsl.sh
+++ build_qsm_fsl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-imageName='tgv_fsl_5p0p11'
+imageName='tgvqsm_fsl_5p0p11'
 buildDate=`date +%Y%m%d`
 
 #install neurodocker
@@ -8,32 +8,41 @@ buildDate=`date +%Y%m%d`
 
 #upgrade neurodocker
 #pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --upgrade
+#or
+#pip install --no-cache-dir https://github.com/stebo85/neurodocker/tarball/master --upgrade
+
+
 
 neurodocker generate docker \
-   --base debian:wheezy \
+   --base=debian:stretch \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir /90days /30days /QRISdata /RDS /data /short /proc_temp /TMPDIR /nvme /local /gpfs1" \
-   --install  wget unzip python3 python-numpy python-nibabel python-setuptools cython libdbus-glib-1-2 libjpeg62 libgtk2.0-0  libpng12-0 \
+   --run="locale-gen en_US.UTF-8" \
+   --run="dpkg-reconfigure locales" \
+   --install apt_opts='--quiet' python-setuptools wget unzip python3 python-numpy python-nibabel cython libdbus-glib-1-2 libjpeg62 libgtk2.0-0 \
    --workdir /\
-   --run="wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip" \
-   --run="unzip /TGVQSM-plus.zip" \
+   --run "wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip" \
+   --run "unzip /TGVQSM-plus.zip" \
    --workdir /TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3 \
-   --run="python3 setup.py install" \
-   --dcm2niix version='latest' \
-   -e FSLOUTPUTTYPE=NIFTI_GZ \
+   --run "python setup.py install" \
+   --dcm2niix version=latest method=source \
+   --fsl version=5.0.11 \
+   --env FSLOUTPUTTYPE=NIFTI_GZ \
    --user=neuro \
    > Dockerfile.${imageName}
 
 
-   #--fsl version=5.0.11 \
 
 docker build -t ${imageName}:$buildDate -f  Dockerfile.${imageName} .
 
 #test:
 docker run -it ${imageName}:$buildDate
 #exit 0
+
+
+
 
 
 docker tag ${imageName}:$buildDate caid/${imageName}:$buildDate

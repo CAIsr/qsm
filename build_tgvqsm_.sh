65524c83e021e03d5a79234d43f2ce93bcb0b7fc
Steffen <stebo85@web.de>
2018-12-12 14:31:09 +1000

    auto commit after build run

--- build_tgvqsm_.sh
+++ build_tgvqsm_.sh
@@ -35,7 +35,7 @@ echo "building $imageName"
 #pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --upgrade
 
 neurodocker generate docker \
-   --base=debian:stretch \
+   --base=debian:jessie-backports \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
